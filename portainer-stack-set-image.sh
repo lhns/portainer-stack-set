2ed02646cbc9ef7e33a715b8cc8546116235c071
Pierre Kisters <pierrekisters@gmail.com>
2020-02-27 11:42:07 +0100

    cleanup

--- portainer-stack-set-image.sh
+++ portainer-stack-set-image.sh
@@ -4,56 +4,53 @@
 
 set -eu
 
-HOST=$1
-USERNAME=$2
-PASSWORD=$3
-STACKNAME=$4
-IMAGE=$5
+HOST="$1"
+USERNAME="$2"
+PASSWORD="$3"
+STACKNAME="$4"
+IMAGE="$5"
 
 # GENERATE LOGIN TOKEN
-PAYLOAD="{
-  \"username\":\"$USERNAME\",
-  \"password\":\"$PASSWORD\"
-}"
+PAYLOAD="$(jq -nc \
+  --arg username "$USERNAME" \
+  --arg password "$PASSWORD" \
+  '{
+    username: $username,
+    password: $password
+  }'
+)"
 
-LOGIN_TOKEN=$(
-  curl -s -H "Content-Type: application/json" -d "$PAYLOAD" -X POST "$HOST/api/auth" |
-    jq -r .jwt
-)
+LOGIN_TOKEN="$(curl -s -H "Content-Type: application/json" -d "$PAYLOAD" -X POST "$HOST/api/auth" | jq -r '.jwt')"
 
 # GET STACK ID OF $NAME
-ID=$(
+ID="$(
   curl -s -H "Authorization: Bearer $LOGIN_TOKEN" "$HOST/api/stacks" |
-    jq -c ".[] | select( .Name==(\"$STACKNAME\"))" |
-    jq -r .Id
-)
+    jq -r --arg stackname "$STACKNAME" 'map(select(.Name == $stackname).Id)[]'
+)"
 
-STACK=$(curl -s -H "Authorization: Bearer $LOGIN_TOKEN" "$HOST/api/stacks/$ID")
-
-# GET THE ENV
-ENV=$(printf '%s' "$STACK" | jq .Env)
+STACK="$(curl -s -H "Authorization: Bearer $LOGIN_TOKEN" "$HOST/api/stacks/$ID")"
 
 # GET THE ENDPOINT ID
-ENDPOINT_ID=$(printf '%s' "$STACK" | jq .EndpointId)
+ENDPOINT_ID="$(jq -nr --argjson stack "$STACK" '$stack.EndpointId')"
 
 # GET THE STACK LIVE FILE
-STACKFILE=$(
-  curl -s -H "Authorization: Bearer $LOGIN_TOKEN" "$HOST/api/stacks/$ID/file" |
-    jq .StackFileContent
-)
+STACKFILE="$(curl -s -H "Authorization: Bearer $LOGIN_TOKEN" "$HOST/api/stacks/$ID/file" | jq -r '.StackFileContent')"
 
-NEW_STACKFILE=$(
-  printf '%s' "$STACKFILE" | jq --arg image "$IMAGE" 'gsub("(?<=image: ).*?(?=\\r?\\n)"; $image)'
-)
+NEW_STACKFILE="$(jq -nr --arg stackfile "$STACKFILE" --arg image "$IMAGE" '$stackfile | gsub("(?<=image: ).*?(?=\\r?\\n)"; $image)')"
+
+PAYLOAD="$(jq -nc \
+  --arg new_stackfile "$NEW_STACKFILE" \
+  --argjson stack "$STACK" \
+  '{
+      StackFileContent: $new_stackfile,
+      Env: $stack.Env,
+      Prune: true
+    }'
+)"
 
 # UPDATE THE STACK > /dev/null beacuse the $ENV contains passwords
-PAYLOAD="{
-  \"StackFileContent\": ${NEW_STACKFILE},
-  \"Env\": ${ENV},
-  \"Prune\": true
-}"
-
-curl -s -H 'Content-Type: text/json; charset=utf-8' \
-        -H "Authorization: Bearer $LOGIN_TOKEN" \
+curl -s \
+     -H 'Content-Type: text/json; charset=utf-8' \
+     -H "Authorization: Bearer $LOGIN_TOKEN" \
      -d "$PAYLOAD" \
      -X PUT "$HOST/api/stacks/$ID?endpointId=$ENDPOINT_ID" > /dev/null
